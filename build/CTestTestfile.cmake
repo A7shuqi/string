e1455858998e4c94b11ee6a6d58597d5b5ea6316
zk <zk@zk.com>
2022-07-04 17:53:31 +0800

    stringOp

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/zk/vswork/string
+# Build directory: /home/zk/vswork/string/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ source/main.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file main.cpp
+ * @author zk (zk@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-07-04
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+#include<string>
+using namespace  std; 
+
+int main(int argc, char *argv[])
+{
+       string str;
+       str.append("12321321");
+       cout <<str.c_str()<<str.length();
+       return 0;
+}
